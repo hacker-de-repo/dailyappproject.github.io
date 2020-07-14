653bcb3c783fa6523fa3dd6812f046b260a4938f
mateus <mateusmansour@gmail.com>
2020-07-14 11:28:05 -0300

    remove hash

--- js/main.js
+++ js/main.js
@@ -1,9 +1,43 @@
-$(document).ready(function() {
-    $('.subir').click(function(){
-        $('html, body').animate({scrollTop:0}, 'slow');
-        return false;
-    });
-});
-document.querySelector('html').onclick = function() {
-    alert('Ai! Pare de me cutucar!');
+
+// ==UserScript==
+// @name        Medium: remove location hash
+// @namespace   http://efcl.info/
+// @description Remove location hash from medium
+// @include     https://medium.com/*#*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+function removeLocationHash(){
+    var noHashURL = window.location.href.replace(/#.*$/, '');
+    window.history.replaceState('', document.title, noHashURL) 
 }
+window.addEventListener("popstate", function(event){
+    removeLocationHash();
+});
+window.addEventListener("hashchange", function(event){
+    event.preventDefault();
+    removeLocationHash();
+});
+window.addEventListener("load", function(){
+    removeLocationHash();
+});
+function clicou() {
+    alert("funcionou");
+}
+jQuery(document).ready(function() {
+    // Exibe ou oculta o botão
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > 200) {
+            jQuery('.voltar-ao-topo').fadeIn(200);
+        } else {
+            jQuery('.voltar-ao-topo').fadeOut(200);
+        }
+    });
+    
+    // Faz animação para subir
+    jQuery('.voltar-ao-topo').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, 300);
+    })
+});
